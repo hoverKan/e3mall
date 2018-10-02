32dad873f88aef024e2bf098712d49f7a76434fe
HoverKan <xinghua_glb@kan.com>
2018-10-02 10:18:27 +0800

    架构改为SOA，使用dubbo中间件。完成后台查询商品列表0

---
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -0,0 +1,39 @@
+package cn.e3mall.controller;
+
+import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 商品管理Controller
+ *
+ * @author hoverkan
+ * @create 2018-10-01 16:16
+ */
+@Controller
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("item/{itemId}")
+    @ResponseBody
+    public TbItem getItemById(@PathVariable long itemId){
+        TbItem item = itemService.getItemById(itemId);
+        return item;
+    }
+
+    @RequestMapping("/item/list")
+    @ResponseBody
+    public EasyUIDataGridResult getItemList(Integer page,Integer rows){
+        // 调用服务查询商品列表
+        EasyUIDataGridResult result = itemService.getItemList(page, rows);
+        return result;
+    }
+
+}
